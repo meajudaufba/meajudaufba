98085d71cdd512217824682f2aa219b39c3d84d1
Rodrigo Araujo <dygufa@gmail.com>
2016-06-08 17:10:43 -0300

    Adding user interface and express

--- server.js
+++ server.js
@@ -1,21 +1,62 @@
-var Ufba = require('./api/ufba.js');
+var express 		= require('express'),
+	bodyParser      = require('body-parser'),
+	app 			= express(),
+	Ufba 			= require('./api/ufba/ufba.js');
 
-var Ufba = require('./api/ufba/ufba.js');
-var user = new Ufba({
-	username: process.env.USERNAME,
-	password: process.env.PASSWORD,
+var PORT = 5080;
+
+// Get base directory
+var cwd = process.cwd();
+
+// to support JSON-encoded bodies
+app.use(bodyParser.json());       
+// to support URL-encoded bodies
+app.use(bodyParser.urlencoded({extended: true})); 
+
+app.use(express.static(cwd + '/static'));
+
+app.get('/', function(req, res) {
+	res.sendFile(cwd + '/static/index.html');
 });
 
-user.login(function(logged) {
-	if (logged) {
-		user.getWelcome(function(res) {
-			console.log(res);
-		});
-		user.getGrades(function(res) {
-			console.log(res);
-		});
-		user.getMajorInformation(function(res) {
-			console.log(res);
-		});
-	}
+app.post('/login', function(req, res) {
+	var formData        = req.body;
+
+	var user = new Ufba({
+		username: formData.cpf,
+		password: formData.password,
+	});
+
+	user.login(function(logged) {
+		if (logged) {
+			user.getWelcome(function(welcome) {
+				res.json({
+					ok: true,
+					data: welcome
+				});
+			});
+			/*user.getGrades(function(res) {
+				//console.log(res);
+			});
+			user.getMajorInformation(function(res) {
+				///console.log(res);
+			});*/
+		} else {
+			res.json({
+				ok: false,
+				data: {
+					msg: 'CPF ou senha inválidos.'
+				}
+			});
+		}
+	});
+});
+
+
+app.listen(PORT, function (err) {
+    if (err) {
+        console.error(err)
+    } else {
+        console.log('App is ready at : ' + PORT);
+    }
 });
