96e91ca3fddb341ab39d9ded9744ec52c945ab22
Rodrigo Araujo <dygufa@gmail.com>
2016-09-05 20:57:35 -0300

    just trying to get rid of one level of braces

--- app/core/routes.js
+++ app/core/routes.js
@@ -25,27 +25,26 @@ fs.readdirSync(controllers_path).forEach(function (file) {
 function verifyToken(req, res, next) {
 	var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
-	if (token) {
-	    jwt.verify(token, TOKEN_SECRET, function(err, decoded) {
-	        if (err) {
-	            return res.json({
-	                success: false,
-	                message: 'Failed to authenticate token.',
-	                code: 2001
-	            });
-	        } else {
-	            // if everything is good, save to request for use in other routes
-	            req.decoded = decoded;
-	            next();
-	        }
-	    });
-	} else {
-	    return res.status(403).send({
+	if (!token) {
+		return res.status(403).send({
 	        success: false,
 	        message: 'No token provided.',
 	        code: 2002
 	    });
 	}
+
+    jwt.verify(token, TOKEN_SECRET, function(err, decoded) {
+        if (!err) {
+        	req.decoded = decoded;
+            return next();
+        }
+
+        return res.json({
+            success: false,
+            message: 'Failed to authenticate token.',
+            code: 2001
+        });
+    });
 }
 
 routes.post('/login', controllers.users.login);
